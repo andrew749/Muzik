238559a6a69a4e1555afc05c86cd0b7023badf98
Andrew <andrewcod749@gmail.com>
2014-09-05 23:55:40 -0400

    can successfully write to a file

---
+++ app/src/main/java/com/acod/play/app/XMLParser.java
@@ -0,0 +1,95 @@
+package com.acod.play.app;
+
+import android.content.Context;
+import android.util.Xml;
+
+import com.acod.play.app.Models.SongResult;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+
+/**
+ * Created by Andrew on 9/5/2014.
+ */
+public class XMLParser {
+    Context context;
+
+    public XMLParser(Context context) {
+        this.context = context;
+    }
+
+    public void writeToXML(ArrayList<SongResult> results) throws IOException {
+        String filename = "PlaySave" + System.currentTimeMillis();
+        String string = createXml(results);
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String createXml(ArrayList<SongResult> results) throws IOException {
+        XmlSerializer serializer = Xml.newSerializer();
+        StringWriter writer = new StringWriter();
+        serializer.setOutput(writer);
+        serializer.startDocument("UTF-8", true);
+        for (SongResult result : results) {
+            serializer.startTag("", "entry");
+            serializer.startTag("", "name");
+            serializer.text(result.getName());
+            serializer.endTag("", "name");
+            serializer.startTag("", "url");
+            serializer.text(result.url);
+            serializer.endTag("", "url");
+            serializer.endTag("", "entry");
+        }
+        serializer.endDocument();
+        return writer.toString();
+    }
+
+    public ArrayList<SongResult> readFromXML(InputStream in) throws XmlPullParserException, IOException {
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        XmlPullParser parser = factory.newPullParser();
+        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(in, null);
+
+        return parseXml(parser);
+    }
+
+    private ArrayList<SongResult> parseXml(XmlPullParser parser) throws XmlPullParserException, IOException {
+        int eventType = parser.getEventType();
+        ArrayList songresults = new ArrayList();
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            String name = null;
+            switch (eventType) {
+
+
+                case XmlPullParser.START_TAG:
+                    name = parser.getName();
+                    if (name == "entry") {
+                        SongResult result = new SongResult(parser.nextText(), parser.nextText());
+                        songresults.add(result);
+
+                    }
+                    eventType = parser.next();
+                    break;
+            }
+
+        }
+        return songresults;
+    }
+
+
+}
